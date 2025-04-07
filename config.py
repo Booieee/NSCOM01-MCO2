0309ecc394e42e67e13a6f2f3a2f69e214f6c5fc
Andrei Zarmin De Jesus <161214656+DreiZzzzz@users.noreply.github.com>
2025-04-07 21:51:55 +0800

    fix directories

--- config.py
+++ config.py
@@ -1,12 +1,8 @@
-'''
-define constants for SIP and RTP settings
-'''
-
-SIP_PORT = 5060
-RTP_PORT = 5004
+LOCAL_IP = "127.0.0.1"  # or your LAN IP
+SIP_PORT = 5060         # default or any valid SIP ports
+RTP_PORT = 5004        # default or any valid RTP ports // ANY EVEN NUMBER PORTS
 RTP_VERSION = 2
 PAYLOAD_TYPE = 0  # PCMU
 SSRC = 12345 # Synchronization source identifier
-LOCAL_IP = "127.0.0.1"
 AUDIO_CODEC = "PCMU"  # G.711 codec
 BUFFER_SIZE = 1024  # Buffer size for SIP
